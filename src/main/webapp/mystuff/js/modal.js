d115d1fe68f859119a7f2feb8af5eb3408059fac
Json.Jeong <Json.Jeong@JsonJeong-book>
2017-02-19 23:13:53 +0900

    좋아요 기능 구현

--- src/main/webapp/mystuff/js/modal.js
+++ src/main/webapp/mystuff/js/modal.js
@@ -10,16 +10,28 @@ $( function() {
         }, 1000); 
     });
 });
-//
-//$( function() { 
-//	$(document.body).on( "click", ".like-btn", function() { 
-//		$( ".person" ).removeClass("animated zoomOut");
-//		$( ".person" ).addClass("animated zoomOut");
-//		$( ".person-back" ).css("animation-duration", "0.5s");
-//		$( ".modal-content" ).css("animation-delay", "0.1s");
-//		$( ".modal-content" ).css("animation-duration", "0.8s");
-//		setTimeout( function() {
-//			$('.person').remove();
-//		}, 1000); 
-//	});
-//});
+
+$( ".like-btn" ).hover(
+	  function() {
+	    $( this ).addClass( "animated jello" );
+	    console.log(this);
+	  }, 
+	  function() {
+	    $( this ).removeClass( "animated jello" );
+	  }
+	);
+	
+var likeState = false; 
+$( function() { 
+	$(document.body).on( "click", ".like-btn", function() { 
+		if (!likeState) {
+			likeState = true; 
+			$( "#like-n-btn").css("display", "none");
+			$( "#like-c-btn").css("display", "inline-block");
+		} else {
+			likeState = false; 
+			$( "#like-c-btn").css("display", "none");
+			$( "#like-n-btn").css("display", "inline-block");
+		}
+	});
+});
