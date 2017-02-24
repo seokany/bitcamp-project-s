7b767770cbf1837274e611a1ae65193f05b29a79
Json.Jeong <Json.Jeong@JsonJeong-book>
2017-02-24 09:34:50 +0900

    .chat-form .animate() 적용

--- src/main/webapp/mentos/js/chat.js
+++ src/main/webapp/mentos/js/chat.js
@@ -1,11 +1,16 @@
-$(".whole_box").scroll(function () {
-	/*
-	if ($(".whole_box").scrollTop() >= 10) {
-		 $(".planning-box").slideUp("slow");
-	}
-	if ($(".whole_box").scrollTop() <= 5) {
-		 $(".planning-box").slideDown("slow");
-	}
-	*/
+$(".chat-form").hover(
+	function () {
+		$(".chat-form").animate({
+			height: "30vmin"
+		}, 400);
+		$(".chat-form textarea").animate({
+			height: "25vmin"
+		}, 600),
+		$(".chatbox").css("padding-top", "30vmin"); 
+	},
+	function () {
+		$(".chatbox").css("padding-top", "60vmin"); 
+		$(".chat-form").css("height", "50px");
+		$(".chat-form textarea").css("height", "30px");
 });
 
