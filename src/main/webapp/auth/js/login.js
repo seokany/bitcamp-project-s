b8d7dcca9bce12b91bb002cba89d53bb02f3519f
jhwani <jhwani@DESKTOP-31AJF84>
2017-02-26 18:41:36 +0900

    로그인 폼 완료

---
+++ src/main/webapp/auth/js/login.js
@@ -0,0 +1,80 @@
+$(document).ready(function() {
+  
+  var animating = false,
+      submitPhase1 = 1100,
+      submitPhase2 = 400,
+      logoutPhase1 = 800,
+      $login = $(".login"),
+      $app = $(".app");
+  
+  function ripple(elem, e) {
+    $(".ripple").remove();
+    var elTop = elem.offset().top,
+        elLeft = elem.offset().left,
+        x = e.pageX - elLeft,
+        y = e.pageY - elTop;
+    var $ripple = $("<div class='ripple'></div>");
+    $ripple.css({top: y, left: x});
+    elem.append($ripple);
+  };
+  
+  $(document).on("click", ".login_submit", function(e) {
+    if (animating) return;
+    animating = true;
+    var that = this;
+    ripple($(that), e);
+    $(that).addClass("processing");
+    setTimeout(function() {
+      $(that).addClass("success");
+      setTimeout(function() {
+        $app.show();
+        $app.css("top");
+        $app.addClass("active");
+      }, submitPhase2 - 70);
+      setTimeout(function() {
+        $login.hide();
+        $login.addClass("inactive");
+        animating = false;
+        $(that).removeClass("success processing");
+      }, submitPhase2);
+    }, submitPhase1);
+  });
+  
+  $(document).on("click", ".app_logout", function(e) {
+    if (animating) return;
+    $(".ripple").remove();
+    animating = true;
+    var that = this;
+    $(that).addClass("clicked");
+    setTimeout(function() {
+      $app.removeClass("active");
+      $login.show();
+      $login.css("top");
+      $login.removeClass("inactive");
+    }, logoutPhase1 - 120);
+    setTimeout(function() {
+      $app.hide();
+      animating = false;
+      $(that).removeClass("clicked");
+    }, logoutPhase1);
+  });
+  
+  $(document).on("click", ".new-sign", function(e) {
+	  $(".sign-in").addClass("animated fadeOutLeft");
+	  $(".sign-up").removeClass("animated fadeOutRight");
+	  $(".sign-up").addClass("animated fadeInRight");
+	$('.sign-up').css("display", "block");
+//	$('.sign-in').css("display", "none");
+	  
+  })
+  
+    $(document).on("click", ".close", function(e) {
+    	$(".sign-up").addClass("animated fadeOutRight");
+    	 $(".sign-in").removeClass("animated fadeOutLeft");
+    	 $(".sign-in").addClass("animated fadeInLeft");
+    	 $('.sign-in').css("display", "block");
+//	$('.sign-up').css("display", "none");
+	  
+  })
+  
+});
