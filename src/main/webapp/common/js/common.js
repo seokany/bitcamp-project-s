aac04115f9c1cfa4b263dc1a30a0469291d279cb
Json.Jeong <Json.Jeong@JsonJeong-book>
2017-03-07 15:18:07 +0900

    mbti 1차 완성

--- src/main/webapp/common/js/common.js
+++ src/main/webapp/common/js/common.js
@@ -9,10 +9,10 @@ $( function() {
 			windowWidth = $(window).width();
 			windowHeigth = $(window).height();
 			
-			$(".main-frame").css("height", windowHeigth + "px");
+//			$(".main-frame").css("height", windowHeigth + "px");
 		});
 			
-		$(".main-frame").css("height", windowHeigth + "px");
+//		$(".main-frame").css("height", windowHeigth + "px");
 	});
 	
 	/* header 호출 스크립트 */
@@ -24,4 +24,37 @@ $( function() {
 	$.get("common/footer.html", function(result) {
 		$("#footer").html(result); 
 	});
+	
+	var wheelState;
+	var wheelType;
+	
+	$("html, body").on("mousewheel DOMMouseScroll", function(event) {
+		wheelState = true; 
+		wheelType = event.originalEvent.wheelDelta;
+	});
+		
+	function hasScrolled() {
+		if (wheelType < 0) {
+			$(".frame-left").addClass("old-left");
+			$(".frame-center").switchClass("frame-center", "frame-left", 2000, "easeInOutBack");
+			$(".frame-right").switchClass("frame-right", "frame-center", 2000, "easeInOutBack");
+			$(".frame-none").switchClass("frame-none", "frame-right", 0, "easeInOutBack");
+			$(".old-left").switchClass("frame-left", "frame-none", 2000, "easeInOutBack");
+			$(".old-left").removeClass("old-left");
+		} else {
+			$(".frame-right").addClass("old-right");
+			$(".frame-center").switchClass("frame-center", "frame-right", 1000, "easeInOutBack");
+			$(".frame-right").switchClass("frame-right", "frame-none", 1000, "easeInOutBack");
+			$(".frame-none").switchClass("frame-none", "frame-left", 0, "easeInOutBack");
+			$(".old-right").switchClass("frame-right", "frame-none", 1000, "easeInOutBack");
+			$(".old-right").removeClass("old-right");
+		}
+	};
+		
+	setInterval(function() {
+		if (wheelState) {
+			hasScrolled(); 
+			wheelState = false; 
+		}
+	}, 250);
 });
