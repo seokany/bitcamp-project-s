1623d14d25e54b3a005b96287926ae9ee1f3be2b
Caroline-Cheon <caroline.jy.cheon@gamil.com>
2017-03-08 17:56:58 +0900

    domain 작업

---
+++ src/main/java/bitcamp/java89/ems2/domain/plan.java
@@ -0,0 +1,21 @@
+package bitcamp.java89.ems2.domain;
+
+public class plan {
+  
+  protected Object planMap;
+  protected Object planImage;
+  
+  public Object getPlanMap() {
+    return planMap;
+  }
+  public void setPlanMap(Object planMap) {
+    this.planMap = planMap;
+  }
+  public Object getPlanImage() {
+    return planImage;
+  }
+  public void setPlanImage(Object planImage) {
+    this.planImage = planImage;
+  }
+  
+}
---
+++ src/main/java/bitcamp/java89/ems2/domain/qna.java
@@ -0,0 +1,15 @@
+package bitcamp.java89.ems2.domain;
+
+public class qna {
+  
+  protected Object chatting;
+
+  public Object getChatting() {
+    return chatting;
+  }
+
+  public void setChatting(Object chatting) {
+    this.chatting = chatting;
+  }
+  
+}
