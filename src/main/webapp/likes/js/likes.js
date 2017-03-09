64b4148a6482cdf01fef67ba00670649c604ec0e
Json.Jeong <Json.Jeong@JsonJeong-book>
2017-03-09 10:29:17 +0900

    java/resources/dreamtree.sql, S_4.exerd 관리 대상에 포함 .gitignore 업데이트 likes/main 작업 진행중 업데이트

--- src/main/webapp/likes/js/likes.js
+++ src/main/webapp/likes/js/likes.js
@@ -1,36 +1,74 @@
 $(function() {
 	$(".thumb").on("click", function() {
+		console.log($(this).data("css"));
 		$(".thumb").each(function() {
+			$(this).css("display", "none");
+		});
+		$(".desc").each(function() {
 			$(this).css("display", "none");
 		});
 		$(this).css({
 			display: "block",
 			position: "relative",
 			margin: "0 20px",
-			"z-index": "9"
-
+			"box-shadow": "10px 10px 5px rgba(132, 132, 132, .4)"
 		});
-		if ($(this).attr("class") == "thumb two") $(this).css("left", "324px");
-		if ($(this).attr("class") == "thumb thr") $(this).css("left", "634px");
-		if ($(this).attr("class") == "thumb fou") $(this).css("left", "924px");
+		$(".sub-board").css({
+			display: "inline-block",
+			position: "relative",
+			top: "-620px",
+			left: "500px", 
+			width: "710px",
+			height: "610px",
+			overflow: "hidden",
+		});
+		if ($(this).hasClass("one")) {
+			$(".sub-board .one").css({
+				width: "710px",
+				left: "-500px",
+				display: "block",
+			});
+			$(".sub-board .one").animate({
+				left: "10px",
+			}, 1000);
+		} else if ($(this).hasClass("two")) {
+			$(this).css("left", "324px");
+			$(".sub-board .two").css({
+				width: "710px",
+				left: "-500px",
+				display: "block",
+			});
+			$(".sub-board .two").animate({
+				left: "10px",
+			}, 1000);
+		} else if ($(this).hasClass("thr")) {
+			$(this).css("left", "634px");
+			$(".sub-board .thr").css({
+				width: "710px",
+				left: "-500px",
+				display: "block",
+			});
+			$(".sub-board .thr").animate({
+				left: "10px",
+			}, 1000);
+		} else if ($(this).hasClass("fou")) {
+			$(this).css("left", "944px");
+			$(".sub-board .fou").css({
+				width: "710px",
+				left: "-500px",
+				display: "block",
+			});
+			$(".sub-board .fou").animate({
+				left: "10px",
+			}, 1000);
+		}
 		
 		$(this).animate({
 			left: "50px",
 			top: "-50px",
 			width: "360px", 
-			height: "660px"
+			height: "660px",
 		}, 500);
 		
-		$(this).children(".person-desc").css({
-			display: "block",
-		    position: "relative",
-		    top: "-650px",
-		    width: "500px",
-			"z-index": "1"
-		});
-		
-		$(this).children(".person-desc").animate({
-			left: "450px"
-		}, 1000);
 	});
 });
