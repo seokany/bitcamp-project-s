6e3511a1fd43a22a696000ee622e2dee483c87d1
CAROLINE <CAROLINE@DESKTOP-3R3CKAR>
2017-03-17 20:17:33 +0900

    토글메뉴 (바디토글시 접히는 기능)

--- src/main/webapp/common/js/header.js
+++ src/main/webapp/common/js/header.js
@@ -1,62 +1,47 @@
 /*토글메뉴*/
 
-$(function () {
-
+$(function() {
+  
   var isopen_usermenu = false;
   var isopen_messagemenu = false;
   
-  /* Open and close usermenu event*/
-  $(".user-menu-toggle").on("click", function () {
-    if(!isopen_usermenu) {
-      
-      // Show menu
-      $(".message-menu").hide();
-      $(".user-menu").show();
-
-      isopen_usermenu = true;
-      isopen_messagemenu = false;
-    } else {
-      
-      // Close menu
-		$(".user-menu").hide();
-
-      isopen_usermenu = false;
-    } 
-  });
-  
-  $(".message-menu-toggle").on("click", function () {
-    if(!isopen_messagemenu) {
-
-      // Show menu
-      $(".user-menu").hide();
-      $(".message-menu").show();
-
-      isopen_messagemenu = true;
-      isopen_usermenu = false;
-    } else {
-
-      // Close menu
-      $(".message-menu").hide();
-
-      isopen_messagemenu = false;
-    } 
-  });
-  
-  
-/*  $("body").on("click", function (e) {
-    if(e.target.className != "user-menu-toggle" && e.target.className != "message-menu-toggle" 
-      && e.target.className != "sing-in on") {
-      
-      console.log(e.target.className);
-      // Close menu
+  $(document.body).on("click", function(e) {
+    var target = $(e.target); 
+    console.log(e.target.classList[0]);
+    if (!target.parent().hasClass("toggle")) {
       $(".user-menu").hide();
       $(".message-menu").hide();
-
-      isopen_usermenu = false;
-      isopen_messagemenu = false;
-    } 
-  });*/
-
+    } else {
+      if (e.target.classList[0] == "menu-btn") {
+        
+        if (!isopen_usermenu) {
+        $(".message-menu").hide();
+        $(".user-menu").show();
+        isopen_usermenu = true;
+        isopen_messagemenu = false;
+        
+      } else {
+            $(".user-menu").hide();
+            isopen_usermenu = false;
+          }
+      }
+    
+      if (e.target.classList[0] == "message-btn") {
+        
+        if (!isopen_messagemenu) {
+        $(".user-menu").hide();
+        $(".message-menu").show();
+        isopen_messagemenu = true;
+        isopen_usermenu = false;
+        
+        } else {
+            $(".message-menu").hide();
+            isopen_messagemenu = false;
+        }
+      }
+      
+    }
+  });
   
 });
 
