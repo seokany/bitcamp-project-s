e3a9a6255709ebf5532c4793519e0c2de1f74edc
Json.Jeong <Json.Jeong@JsonJeong-book>
2017-03-19 02:14:44 +0900

    expert/driver.html 1차 완성

--- src/main/webapp/common/js/header.js
+++ src/main/webapp/common/js/header.js
@@ -1,48 +1,39 @@
 /*토글메뉴*/
 
 $(function() {
-  
   var isopen_usermenu = false;
   var isopen_messagemenu = false;
   
   $(document.body).on("click", function(e) {
     var target = $(e.target); 
-    console.log(e.target.classList[0]);
     if (!target.parent().hasClass("toggle")) {
       $(".user-menu").hide();
       $(".message-menu").hide();
     } else {
       if (e.target.classList[0] == "menu-btn") {
-        
         if (!isopen_usermenu) {
         $(".message-menu").hide();
         $(".user-menu").show();
         isopen_usermenu = true;
         isopen_messagemenu = false;
-        
       } else {
             $(".user-menu").hide();
             isopen_usermenu = false;
           }
       }
-    
       if (e.target.classList[0] == "message-btn") {
-        
         if (!isopen_messagemenu) {
         $(".user-menu").hide();
         $(".message-menu").show();
         isopen_messagemenu = true;
         isopen_usermenu = false;
-        
         } else {
             $(".message-menu").hide();
             isopen_messagemenu = false;
         }
       }
-      
     }
   });
-  
 });
 
 
--- src/main/webapp/common/readme.md
+++
@@ -1,5 +0,0 @@
-header
-
-  <div id="header"></div>
-  <link rel="stylesheet" href="../css/common/common.css">
-  <script type="text/javascript" src="../js/common/common.js"></script>
