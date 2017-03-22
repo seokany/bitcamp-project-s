8e4cfbcd921834e132a852b0ce1dde0afca5e2e9
Json.Jeong <Json.Jeong@JsonJeong-book>
2017-03-22 21:29:13 +0900

    클릭 및 휠 이벤트 제어를 위한 글로벌 변수 선언

--- src/main/webapp/common/js/context.js
+++ src/main/webapp/common/js/context.js
@@ -1,2 +1,7 @@
 var serverRoot = 'http://localhost:8080/bitcamp-project-s';
 var clientRoot = '/bitcamp-project-s'
+
+/*   click 이벤트 조건 제어를 위한 글로벌 변수   */
+var loginEvent = false;
+var testEvent = true;
+/*   /click 이벤트 조건 제어를 위한 글로벌 변수   */
