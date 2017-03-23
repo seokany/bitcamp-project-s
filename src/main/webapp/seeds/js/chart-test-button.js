86e5c0a1a0078e81147e60d640042e87a4cfc5a3
Caroline-Cheon <caroline.jy.cheon@gamil.com>
2017-03-23 21:59:31 +0900

    버튼 작업중

---
+++ src/main/webapp/seeds/js/chart-test-button.js
@@ -0,0 +1,14 @@
+/*$('.share-alt').on('click', function(e){
+	$(".branch").removeClass("no-animation");
+	$('.branch').toggleClass("open");
+});
+
+$('.social').on('click', function(e){
+	$(".container").removeClass("no-animation");
+	$(".container").toggleClass("open");
+});
+
+$('.share').on('click', function(e){
+	$(".float-circle").removeClass("no-animation");
+	$(".float-circle").toggleClass("open");
+});*/
